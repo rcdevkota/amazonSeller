1a48ab914b734288b204721b663223ece5a7c04a
Alexander Dietsche <ekelalfred.tulpe@gmail.com>
2023-12-26 17:38:58 +0100

    Kommentar zum headers User-Agent

--- amazonSeller/categoryScrape.py
+++ amazonSeller/categoryScrape.py
@@ -5,7 +5,10 @@ import re
 def fetch_amazon_best_sellers():
     url = "https://www.amazon.com/gp/bestsellers/"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"
+        # Hier muss ein Switch passieren auf welchem OS sich aufgehalten wird.
+        }
 
     response = requests.get(url, headers=headers)
     
