b61e7b70babef9410b860e372f1ea3963ae9dab7
John Doe <rcdevkota2011@hotmail.com>
2024-02-15 10:23:05 +0100

    new changes

--- amazonSeller/views/__init__.py
+++
@@ -1,4 +0,0 @@
-# your_app_name/views/__init__.py
-
-from .category_views import *
-from .home_views import *
--- amazonSeller/views/home_views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-
-def home(request):
-    return render(request, 'amazonseller/home.html')
